eec5920d07d56b39819d2ff9c00357cb39e6b21d
mojtabaabedi81 <mojtaba.abedi1381@gmail.com>
2024-03-28 12:13:22 +0330

    add the changes for find the error and some another one

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import speech_recognition as sr
 from googletrans import Translator
+from werkzeug.exceptions import BadRequestKeyError
+
 
 app = Flask(__name__)
 translator = Translator()
@@ -9,24 +11,20 @@ translator = Translator()
 def index():
     return render_template('index.html')
 
-@app.route('/speech-to-text', methods=['POST'])
+@app.route('/speech-to-text/', methods=['POST'])
 def speech_to_text():
     audio_file = request.files['audio']
-    if audio_file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-
-    # Initialize the SpeechRecognition recognizer
     recognizer = sr.Recognizer()
 
     try:
         # Use recognizer to convert speech to text
-        with sr.AudioFile(audio_file) as source:
+        with sr.AudioFile(request.files['audio']) as source:
             audio_data = recognizer.record(source)
             # Recognize speech with Persian language
             text = recognizer.recognize_google(audio_data, language="fa-IR")
             # Translate the text to English for the response
             translated_text = translator.translate(text, src="fa", dest="en").text
-            return jsonify({'text': translated_text}), 200
+            return jsonify({"text": translated_text}), 200
     except sr.UnknownValueError:
         return jsonify({'error': 'Speech not recognized'}), 400
     except sr.RequestError as e:
